d09987ccdc9ba4892c875a501609544dbeb4b973
Periyot <mdsabinov@gmail.com>
2021-11-30 14:06:37 +0300

    NavBar Added

--- web-ui-01/src/App.js
+++ web-ui-01/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './Component/NavBar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <NavBar />
       </div>
     )
   }
