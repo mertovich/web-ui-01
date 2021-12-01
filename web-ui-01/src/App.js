9aacfe0a12a053c70e9e1c26ea3d4a5df823775d
Periyot <mdsabinov@gmail.com>
2021-12-01 13:59:06 +0300

    Register & Basket Router Setting Added

--- web-ui-01/src/App.js
+++ web-ui-01/src/App.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home';
 import Login from './Pages/Login';
+import Basket from'./Pages/Basket'
+import Register from './Pages/Register'
 
 export default class App extends Component {
   render() {
@@ -12,9 +14,10 @@ export default class App extends Component {
          <Routes>
           <Route exact path='/' element={<Home/>}/>
           <Route exact path='login' element={<Login/>}/>
+          <Route exect path='register' element={<Register/>}/>
+          <Route exect path='basket' element={<Basket/>}/>
         </Routes>
       </div>
     )
   }
 }
-
--- web-ui-01/src/Pages/Basket.js
+++ web-ui-01/src/Pages/Basket.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Login extends Component {
+export default class Basket extends Component {
     render() {
         return (
             <div>
-                Login
+                basket
             </div>
         )
     }
